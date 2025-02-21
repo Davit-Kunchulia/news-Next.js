3703971f7cf91a25f343775abe7cefa11e939029
Davit-Kunchulia <148901201+Davit-Kunchulia@users.noreply.github.com>
2025-02-21 17:25:09 +0400

    Fixed .gitignore and removed unneeded files

---
+++ app/news/page.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { DUMMY_NEWS } from "@/dummy-news";
+
+export default function NewsPage() {
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul className="news-list">
+        {DUMMY_NEWS.map((newsItems) => (
+          <li key={newsItems.id}>
+            <Link href={`/news/${newsItems.slug}`}>
+              <img
+                src={`/images/news/${newsItems.image}`}
+                alt={newsItems.title}
+              />
+              <span>{newsItems.title}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
